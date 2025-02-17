49318e65f6af20370ac80e915ac6cf1c5ce84152
PSerdyukov <PSerdyukov@PSERDYUKOV>
2025-02-17 10:47:24 +0300

    Добавлен интерфейс для реализации "Стратегии", выполнение CRUD операций независимо от источника данных

---
+++ CRUDTestTask/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+
+public interface IUserRepository
+{
+    IEnumerable<User> GetAll();
+    User GetById(Guid id);
+    void Add(User user);
+    void Update(User user);
+    void Delete(Guid id);
+}
