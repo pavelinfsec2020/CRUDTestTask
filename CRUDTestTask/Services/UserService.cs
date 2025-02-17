74f5b6cba49f256dd0dd0e8dbce6a80ee949b220
PSerdyukov <PSerdyukov@PSERDYUKOV>
2025-02-17 10:55:51 +0300

    Создан сервис для вызова CRUD операций, в качестве конструктора - наш интерфейс, обьекты будем передавать - реализации его в зависимости от типа данных

---
+++ CRUDTestTask/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿namespace CRUDTestTask.Services
+{
+    public class UserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public IEnumerable<User> GetAllUsers() => _userRepository.GetAll();
+
+        public User GetUserById(Guid id) => _userRepository.GetById(id);
+
+        public void AddUser(User user) => _userRepository.Add(user);
+
+        public void UpdateUser(User user) => _userRepository.Update(user);
+
+        public void DeleteUser(Guid id) => _userRepository.Delete(id);
+    }
+}
