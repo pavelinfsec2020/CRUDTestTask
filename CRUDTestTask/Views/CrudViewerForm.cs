281a8c7f9015319940dafdac9fff317d75af38cd
PSerdyukov <PSerdyukov@PSERDYUKOV>
2025-02-17 09:29:03 +0300

    feat: Создан шаблон проекта

---
+++ CRUDTestTask/Views/CrudViewerForm.cs
@@ -0,0 +1,10 @@
+namespace CRUDTestTask
+{
+    public partial class CrudViewerForm : Form
+    {
+        public CrudViewerForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
